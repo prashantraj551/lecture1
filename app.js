17a7cf9aab6af8bca05796a4f97966ef5867c450
prashantraj551 <prashantraj551gmail.com>
2025-08-10 13:09:29 +0530

    i have change in app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const heading = React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child"},
+       [React.createElement("h1",{id:"parent"},"i'm h1"),React.createElement("h2",{id:"parent"},"i'm h2")]
+    ),React.createElement("div",{id:"child"},
+       [React.createElement("h1",{id:"parent"},"i'm h1"),React.createElement("h2",{id:"parent"},"i'm h2")]
+    )]
+);
+
+console.log(hi);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
